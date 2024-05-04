6b3833fb2b59cf2d9d18e66d5780e20c66d361f6
Mahesh-Kethapaga <107932223+Mahesh-Kethapaga@users.noreply.github.com>
2024-05-04 23:37:19 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.234.231.110:9000/',
+  serverUrl: 'http://54.244.110.229:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
